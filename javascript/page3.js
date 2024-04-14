e4f45739fd37e5c8a147d3fe247915160431a7c6
Shailesh kumar <shaileshkumarsingh988@gmail.com>
2024-04-14 18:18:36 +0530

    added simple auth

--- javascript/page3.js
+++ javascript/page3.js
@@ -1,5 +1,29 @@
 const nextPageButton = document.querySelector("#btn");
 
 nextPageButton.addEventListener("click", () => {
-  window.location.href = "page5.html";
+  const firstName = document.querySelector("#first-name").value.trim();
+  const lastName = document.querySelector("#last-name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+  const checkbox = document.querySelector("#checkbox").checked;
+
+  if (firstName === "" || lastName === "" || email === "" || password === "") {
+    alert("Please fill in all required fields.");
+    return;
+  }
+
+  if (!checkbox) {
+    alert("Please agree to the terms and conditions.");
+    return;
+  }
+
+  if (
+    firstName !== "" &&
+    lastName !== "" &&
+    email !== "" &&
+    password !== "" &&
+    checkbox
+  ) {
+    window.location.href = "page5.html";
+  }
 });
